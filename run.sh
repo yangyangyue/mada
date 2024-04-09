ef25d75d651f0c8a23dfd833888b25e5a7812b27
lily <lily231147@proton.me>
2024-04-10 01:06:46 +0800

    .

--- run.sh
+++ run.sh
@@ -1,22 +1,32 @@
 rm -rf lightning_logs
-# train aada of all appliance from ukdale15
-python train.py --method acave --houses ukdale15 --apps kmdwf
-# train avae of all appliance from ukdale15 
-python train.py --method avae --houses ukdale15 --apps kmdwf
 # train vae from ukdale15
 python train.py --method vae --houses ukdale15 --apps k
 python train.py --method vae --houses ukdale15 --apps m
 python train.py --method vae --houses ukdale15 --apps d
 python train.py --method vae --houses ukdale15 --apps w
 python train.py --method vae --houses ukdale15 --apps f
+# train avae of all appliance from ukdale15 
+python train.py --method avae --houses ukdale15 --apps mdwf
+python train.py --method avae --houses ukdale15 --apps kdwf
+python train.py --method avae --houses ukdale15 --apps kmwf
+python train.py --method avae --houses ukdale15 --apps kmdf
+python train.py --method avae --houses ukdale15 --apps kmdw
+# train aada of all appliance from ukdale15
+# python train.py --method acave --houses ukdale15 --apps kmdwf
 
-# train aada of all appliance from refit256 
-python train.py --method acave --houses refit256 --apps kmdwf
-# train avae of all appliance from refit256 
-python train.py --method avae --houses refit256 --apps kmdwf
+
+
 # train vae from refit256 
 python train.py --method vae --houses refit256 --apps k
 python train.py --method vae --houses refit256 --apps m
 python train.py --method vae --houses refit256 --apps d
 python train.py --method vae --houses refit256 --apps w
 python train.py --method vae --houses refit256 --apps f
+# train avae of all appliance from refit256 
+python train.py --method avae --houses refit256 --apps mdwf
+python train.py --method avae --houses refit256 --apps kdwf
+python train.py --method avae --houses refit256 --apps kmwf
+python train.py --method avae --houses refit256 --apps kmdf
+python train.py --method avae --houses refit256 --apps kmdw
+# train aada of all appliance from refit256 
+# python train.py --method acave --houses refit256 --apps kmdwf
