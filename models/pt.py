d7a59d8a09640a345838911c0f47067f870dc191
lily <lily231147@proton.me>
2024-04-17 22:28:23 +0800

    .

--- models/pt.py
+++ models/pt.py
@@ -8,13 +8,9 @@ email: lily231147@gmail.com
 
 import math
 import torch
-from torch import Tensor, nn
+from torch import nn
 
-from models.common import TransformerEncoder
-
-
-
-
+from models.common import IbnNet, TransformerEncoder
 
 def pe(q_len, d_model, normalize=True):
     pe = torch.zeros(q_len, d_model)
@@ -27,27 +23,48 @@ def pe(q_len, d_model, normalize=True):
         pe = pe / (pe.std() * 10)
     return pe
     
-class PtNet(nn.Module):
-    def __init__(self, channels=256, n_layers=4, window_size=1024) -> None:
+class UpNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.tokenlizer = nn.Linear(16, channels)
-        self.encoder = TransformerEncoder(n_layers)
-        self.detokenlizer = nn.Linear(channels, 16)
-        self.linear = nn.Linear(window_size, window_size)
+        self.ibn = IbnNet(in_channels, out_channels)
+        self.up = nn.ConvTranspose1d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
+        
 
-    def forward(self, _, x: Tensor, gt_apps:Tensor=None):
+    def forward(self, x):
+        y = self.ibn(x)
+        y = self.up(x)
+        return y
+
+class PtNet(nn.Module):
+    def __init__(self, channels=256, n_layers=4, window_size=1024, patch_size=8, patch_stride=4) -> None:
+        super().__init__()
+        self.patch_size = patch_size
+        self.patch_stride = patch_stride
+        n_patch = window_size // patch_stride
+        pad_len = window_size - patch_size - (n_patch - 1) * patch_stride
+        self.padding = nn.ZeroPad1d((0, pad_len))
+        self.tokenlizer = IbnNet(patch_size, channels)
+        self.encoder = TransformerEncoder(n_layers)
+        self.up_nets = nn.Sequential()
+        t_patch_stride = patch_stride
+        while t_patch_stride > 1:
+            self.up_nets.add(UpNet(channels, channels))
+            t_patch_stride >>= 2
+        self.detokenlizer = IbnNet(channels, 1)
+
+    def forward(self, _, x, gt_apps=None):
         """
         Args:
             examples (N, 3, L): input examples
             samples (N, L): input samples
         """
-        x = x.unfold(dimension=-1, size=16, step=16)
-        x = self.tokenlizer(x)
+        x = x.unfold(dimension=-1, size=self.patch_size, step=self.patch_stride).permute(0, 2, 1)
+        x = self.padding(x) # (N, patch_size, n_patch)
+        x = self.tokenlizer(x).permute(0, 2, 1) # (N, n_patch, C)
         x = x + pe(x.shape[1], x.shape[2]).to(x.device)
-        x = self.encoder(x)
-        x = self.detokenlizer(x)
-        x = x.flatten(start_dim=1)
-        x = self.linear(x)
+        x = self.encoder(x).permute(0, 2, 1) # (N, C, n_patch)
+        x = self.up_nets(x) # (N, C, window_size)
+        x = self.detokenlizer(x).squeeze(1) # (N, window_size)
         y = x.relu()
         if self.training:
             return ((gt_apps - y)**2).mean()
