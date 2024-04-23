48c290bee15c093f88ae22133b116b6b90b0c22e
lily <lily231147@proton.me>
2024-04-23 21:28:18 +0800

    .

--- models/aada.py
+++ models/aada.py
@@ -4,17 +4,18 @@ from torch import nn
 from models.attention import AutoEncoder
 
 class AadaNet(nn.Module):
-    def __init__(self, patch_size=8, patch_stride=4, channels_list=(64, 128, 256), window_size=1024, activation=None, conv=True, attn=True, cross=True, bridge='cross', kl=True):
+    def __init__(self, window_size=1024, patch_size=8, patch_stride=4, channels_list=(64, 128, 256), conv=True, attn=True, cross=True, bridge='cross', kl=True, activation=None):
         self.cross, self.kl = cross, kl
         dilation = patch_size // patch_stride
+        activation = nn.ReLU() if activation is None else activation
         def unfold(tensor: torch.Tensor):
             len_pad =  patch_size - patch_stride
             tensor = nn.functional.pad(tensor, (0, len_pad), mode="constant", value=0)
             tensor = tensor.unfold(dimension=-1, size=patch_size, step=patch_stride)
             return tensor.permute(0, 2, 1)
         self.unfold = unfold
-        self.ae = AutoEncoder(patch_size, channels_list, patch_size, activation=None, conv=True, attn=True, cross=True, kl=True)
-        self.fold = nn.Sequential(lambda tensor: tensor.permute(0, 2, 1), nn.Flatten(), nn.Linear(dilation * window_size, window_size), nn.ReLU())
+        self.ae = AutoEncoder(patch_size, channels_list, patch_size, conv, attn, cross, bridge, kl, activation)
+        self.fold = nn.Sequential(lambda tensor: tensor.permute(0, 2, 1), nn.Flatten(), nn.Linear(dilation * window_size, window_size), activation)
     
     def forward(self, x, context=None, y_hat=None):
         # unfold x: patch_size patch_num
