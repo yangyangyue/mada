6291f10ca1b6d608689fc2d68ef05e1321368fe4
lily <lily231147@proton.me>
2024-06-06 22:16:26 +0800

    .

--- models/aada.py
+++ models/aada.py
@@ -15,17 +15,14 @@ class AadaNet(nn.Module):
         super().__init__()
         self.fusion, self.kl = fusion, kl
         activation = nn.ReLU() if activation is None else activation
-        self.ae = AutoEncoder(channels, z_channels, n_layers, conv, attn, fusion, bridge, kl, softmax, activation)
+        self.ae = AutoEncoder(1, channels, z_channels, n_layers, conv, attn, fusion, bridge, kl, softmax, activation)
     
     def forward(self, x, context=None, y_hat=None):
-        # unfold x and context to shape (N, patch_size, window_size//patch_stride)
-        x = self.unfold(x) 
-        if self.fusion: context = self.unfold(context)
         # feed x to autoencoder, don't change the shape
-        if self.kl: x, mu, logvar = self.ae(x, context)
-        else: x = self.ae(x, context)
+        if self.kl: x, mu, logvar = self.ae(x[:, None, :], context[:, None, :])
+        else: x = self.ae(x[:, None, :], context[:, None, :])
         # fold x and make output using a linear layer
-        y = self.fold(x)
+        y = x.relu().flatten()
         if self.training:
             loss = ((y-y_hat) ** 2).mean()
             if self.kl: loss += (-0.5 * (1 + logvar - mu ** 2 - logvar.exp())).sum(dim=(1, 2)).mean()
